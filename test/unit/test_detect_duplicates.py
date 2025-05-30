cd72037173cd8c7c0cb05328f66f3c734c282ecb
David A <david.allert@hotmail.com>
2025-05-30 16:47:42 +0200

    Push.

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -12,17 +12,66 @@ def test_detect_duplicates():
 def temp_file():
   filename = "test.txt"
   with open(filename, "w") as f:
-      f.write("""@article{frattini2023requirements, title={Requirements quality research: a harmonized theory, evaluation, and roadmap}, author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael}, journal={Requirements Engineering}, pages={1--14},year={2023}, publisher={Springer}, doi={10.1007/s00766-023-00405-y}}""")
+      f.write("""@article{frattini2023requirements,
+	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
+	  author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
+	  journal={Requirements Engineering},
+	  pages={1--14},
+	  year={2023},
+	  publisher={Springer},
+	  doi={10.1007/s00766-023-00405-y}
+}
+
+@article{fernandez2017naming,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+  author={M{\'e}ndez Fern{\'a}ndez, Daniel and Wagner, Stefan and Kalinowski, Marcos and Felderer, Michael and Mafra, Priscilla and Vetr{\`o}, Antonio and Conte, Tayana and Christiansson, M-T and Greer, Des and Lassenius, Casper and others},
+  journal={Empirical software engineering},
+  volume={22},
+  number={5},
+  pages={2298--2338},
+  year={2017},
+  publisher={Springer},
+  doi={10.1007/s10664-016-9451-7}
+}
+
+@article{mendez2017naming,
+  title={Naming the pain in requirements engineering: Contemporary Problems, Causes, and Effects in Practice},
+  author={M{\'e}ndez Fern{\'a}ndez, Daniel and Wagner, Stefan and Kalinowski, Marcos and Felderer, Michael and Mafra, Priscilla and Vetr{\`o}, Antonio and Conte, Tayana and Christiansson, M-T and Greer, Des and Lassenius, Casper and others},
+  journal={Empirical software engineering},
+  volume={22},
+  number={5},
+  pages={2298--2338},
+  year={2017},
+  publisher={Springer},
+  doi={10.1007/s10664-016-9451-7}
+}
+
+@article{wagner2019status,
+  title={Status quo in requirements engineering: A theory and a global family of surveys},
+  author={Wagner, Stefan and M{\'e}ndez Fern{\'a}ndez, Daniel and Felderer, Michael and Vetr{\`o}, Antonio and Kalinowski, Marcos and Wieringa, Roel and Pfahl, Dietmar and Conte, Tayana and Christiansson, Marie-Therese and Greer, Desmond and others},
+  journal={ACM Transactions on Software Engineering and Methodology (TOSEM)},
+  volume={28},
+  number={2},
+  pages={1--48},
+  year={2019},
+  publisher={ACM New York, NY, USA}
+}
+
+@article{frattini2023requirements,
+	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
+	  author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
+	  journal={Requirements Engineering},
+	  pages={1--14},
+	  year={2023},
+	  publisher={Springer}
+}""")
 
   yield filename
 
   os.remove(filename)
 
 def test_correct_file_format(temp_file):
-  detect_duplicates(temp_file)
-
-def test_incorrect_file_format():
-  pass
+  assert detect_duplicates(temp_file) == "frattini2023requirements"
 
 def test_incorrect_file_format():
   pass
